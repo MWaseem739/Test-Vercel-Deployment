eecd42e584f46dc21fe88866fc6b565d5d43bafe
WaseemShahid-MQL <89464335+MWaseem739@users.noreply.github.com>
2024-04-25 14:35:43 +0500

    update

--- strapi/my-project/config/plugins.ts
+++ strapi/my-project/config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+export default () => ({
+  "vercel-deploy": {
+    enabled: true,
+    apiToken: "X0SFfiHBsb3xMtoXjbqhI6y7",
+    // ... other plugin configuration options
+  },
+});
