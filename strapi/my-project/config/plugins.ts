49e017e46a1fa93b443979662a560bb68567d3b4
WaseemShahid-MQL <89464335+MWaseem739@users.noreply.github.com>
2024-04-25 14:44:04 +0500

    remove vercel plugin

--- strapi/my-project/config/plugins.ts
+++ strapi/my-project/config/plugins.ts
@@ -1,7 +1,1 @@
-export default () => ({
-  "vercel-deploy": {
-    enabled: true,
-    apiToken: "X0SFfiHBsb3xMtoXjbqhI6y7",
-    // ... other plugin configuration options
-  },
-});
+export default () => ({});
