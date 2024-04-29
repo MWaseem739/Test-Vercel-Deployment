0420a4a6148cedff8445a5cda2b8bbf08db28059
WaseemShahid-MQL <89464335+MWaseem739@users.noreply.github.com>
2024-04-29 12:07:50 +0500

    add strapi deploy plugin

--- strapi/my-project/config/plugins.ts
+++ strapi/my-project/config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default () => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_t0A9A8VwLDGEUF2M14pfFlHAlQdX/JAJLRs684c",
+      apiToken: "VQV8PXEsLKYj7ze4KKlOQB4j",
+      appFilter: "test-vercel-deployment-ey2k",
+      teamFilter: "mwaseem739s-projects",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    },
+  },
+});
